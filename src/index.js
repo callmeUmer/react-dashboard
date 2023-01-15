f33d9ee2cbfd8ae962da027345d6cebcaec2c290
umar <umarsohail43742@gmail.com>
2023-01-15 17:10:21 +0500

    feat: sidebar completed

--- src/index.js
+++ src/index.js
@@ -2,19 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createBrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/">
-    </Route>
-  )
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
