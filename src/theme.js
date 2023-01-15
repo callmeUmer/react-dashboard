622ad43711c63e24c2daef909fe351424712b418
umar <umarsohail43742@gmail.com>
2023-01-15 15:38:09 +0500

    fix: typo fixed in theme

--- src/theme.js
+++ src/theme.js
@@ -20,7 +20,7 @@ export const tokens = (mode) => ({
           100: "#d0d1d5",
           200: "#a1a4ab",
           300: "#727681",
-          400: "#434957",
+          400: "#1F2A40",
           500: "#141b2d",
           600: "#101624",
           700: "#0c101b",
@@ -162,7 +162,7 @@ export const themeSettings = (mode) => {
             },
           }),
     },
-    typograpghy: {
+    typography: {
         fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
         fontSize: 12,
         h1: {
